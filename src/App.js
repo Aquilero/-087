630218caaa746362bc8826a437c281abe6ee4680
Deniz <deniz.durmaz@docc.techstarter.de>
2023-07-31 21:14:24 +0200

    Hausaufgaben Ü087

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Willkommen, UeberMich, Kontakt, Counter, Uhr } from './index';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Deniz's React Komponenten</h1>
       </header>
+      <main>
+        <Willkommen />
+        <UeberMich />
+        <Kontakt />
+        <Counter />
+        <Uhr />
+      </main>
+      <footer>
+        <p>&copy; 2023 Meine Webseite</p>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { useState } from "react";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const Willkommen = () => {
+    return (
+        <section>
+            <h2>Willkommen</h2>
+            <p>Herzlich willkommen auf meiner einfachen Webseite!</p>
+        </section>
+    );
+};
+
+const UeberMich = () => {
+    return (
+        <section>
+            <h2>Über mich</h2>
+            <p>
+                Hier ist eine kurze Beschreibung über mich und meine Interessen.
+            </p>
+        </section>
+    );
+};
+
+const Kontakt = () => {
+    return (
+        <section>
+            <h2>Kontakt</h2>
+            <p>
+                Bei Fragen oder Anregungen können Sie mich gerne kontaktieren:
+                kontakt@meinewebseite.de
+            </p>
+        </section>
+    );
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+function Counter() {
+    const [count, setCount] = React.useState(0);
+
+    const increment = () => setCount(count + 2);
+
+    return (
+        <>
+            <h3>{count}</h3>
+            <button onClick={increment}>+ 2</button>
+        </>
+    );
+}
+
+const Uhr = () => {
+    let time = new Date().toLocaleTimeString()
+
+    const [ctime, setTime] = useState(time)
+    const UpdateTime = () => {
+        time = new Date().toLocaleTimeString()
+        setTime(time)
+    }
+    setInterval(UpdateTime)
+    return <h1>{ctime}</h1>
+}
+
+export { Willkommen, UeberMich, Kontakt, Counter, Uhr };
+
 reportWebVitals();
